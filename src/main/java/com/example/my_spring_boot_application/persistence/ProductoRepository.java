aac95729c47a161d8c76ed5ee2b6651cdb9e0dcb
darwing-21 <darwingarcia2130@gmail.com>
2024-07-24 00:25:54 -0400

    feat(product): add query methods

--- src/main/java/com/example/my_spring_boot_application/persistence/ProductoRepository.java
+++ src/main/java/com/example/my_spring_boot_application/persistence/ProductoRepository.java
@@ -4,11 +4,21 @@ import com.example.my_spring_boot_application.persistence.crud.ProductoCrudRepos
 import com.example.my_spring_boot_application.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
-    public List<Producto> getAll(){
+    public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrdeByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+
     }
 }
--- src/main/java/com/example/my_spring_boot_application/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/example/my_spring_boot_application/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,11 @@ package com.example.my_spring_boot_application.persistence.crud;
 import com.example.my_spring_boot_application.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrdeByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
