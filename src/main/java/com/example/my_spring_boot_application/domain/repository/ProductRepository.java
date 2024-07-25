c7b2f3dffe72cbdb994c8c61ef5066799f0eb15a
darwing-21 <darwingarcia2130@gmail.com>
2024-07-25 16:04:02 -0400

    feat(product): add repository and mapper to domain

---
+++ src/main/java/com/example/my_spring_boot_application/domain/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.example.my_spring_boot_application.domain.repository;
+
+import com.example.my_spring_boot_application.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> getAll();
+
+    Optional<List<Product>> getByCategory(int categoryId);
+
+    Optional<List<Product>> getScarceProduct(int quantity);
+
+    Optional<Product> getProduct(int productId);
+
+    Product save(Product product);
+
+    void delete(int productId);
+}
---
+++ src/main/java/com/example/my_spring_boot_application/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,23 @@
+package com.example.my_spring_boot_application.persistence.mapper;
+
+import com.example.my_spring_boot_application.domain.Category;
+import com.example.my_spring_boot_application.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active")
+
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+}
